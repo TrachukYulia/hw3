01815343a34990a32463d06c53d20b1785e6f1a4
GreenSky <trachukyulia77@gmail.com>
2022-12-03 20:08:15 +0200

    standardized the code

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-'use strict';
+'use strict'
 
-const express = require('express');
+const express = require('express')
 
 // константы
-const port = 80;
-const host = '127.0.0.1';
+const port = 80
+const host = '127.0.0.1'
 
 // приложение
-const app = express();
-app.listen(port);
-app.get("/", (req, res) => {
-  res.sendFile("/index.html");
-});
+const app = express()
+app.listen(port)
+app.get('/', (req, res) => {
+  res.sendFile('/index.html')
+})
 // app.listen(port);
-console.log(`Running on http://${host}:${port}`);
+console.log(`Running on http://${host}:${port}`)
